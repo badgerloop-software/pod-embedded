b7bf7fd64af628f590d3b021ac369e5e5c70cebc
Luke Houge <lukehouge@gmail.com>
2018-11-08 12:29:08 -0600

    styling fixes,  added place holders for focus charts

--- dashboard/public/javascripts/main.js
+++ dashboard/public/javascripts/main.js
@@ -238,8 +238,8 @@ function filterFunction() {
 }
 
 //clones table entry to header (currently IP)
+var x=1
 function clone(id) {
-  var x =1;
   if (x==1){
     setInterval(function(){
       var value = document.getElementById(id).innerHTML;
@@ -248,11 +248,44 @@ function clone(id) {
     }, 300);
     x++;
   }
-  if (x==2){
+  else if (x==2){
     setInterval(function(){
       var value = document.getElementById(id).innerHTML;
       document.getElementById('header_value_2').innerHTML = value;
       document.getElementById('header_label_2').innerHTML = id;
     }, 300);
+    x++;
   }
+  else if (x==3){
+    setInterval(function(){
+      var value = document.getElementById(id).innerHTML;
+      document.getElementById('header_value_3').innerHTML = value;
+      document.getElementById('header_label_3').innerHTML = id;
+    }, 300);
+    x++;
+  }
+  else if (x==4){
+    setInterval(function(){
+      var value = document.getElementById(id).innerHTML;
+      document.getElementById('header_value_4').innerHTML = value;
+      document.getElementById('header_label_4').innerHTML = id;
+    }, 300);
+    x++;
+  }
+  else if (x > 4){
+    alert("Max of 4 values reached, please remove one and try again");
+  }
+}
+
+//clear for focus div
+function clear() {
+  document.getElementById('header_value_1').innerHTML = "";
+  document.getElementById('header_label_1').innerHTML = "Value 1";
+  document.getElementById('header_value_2').innerHTML = "";
+  document.getElementById('header_label_2').innerHTML = "Value 2";
+  document.getElementById('header_value_3').innerHTML = "";
+  document.getElementById('header_label_3').innerHTML = "Value 3";
+  document.getElementById('header_value_4').innerHTML = "";
+  document.getElementById('header_label_4').innerHTML = "Value 4";
+  throw "";
 }
