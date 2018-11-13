407eddbea1537a5cded9238f8656e0648888d36c
Luke Houge <lhouge@nero.cs.wisc.edu>
2018-11-13 15:24:38 -0600

    added comments to the dyanmicStyling.js file to make more clear

--- dashboard/public/javascripts/dynamicStyling.js
+++ dashboard/public/javascripts/dynamicStyling.js
@@ -1,26 +1,26 @@
 //random generator for motion
 setInterval(function(){
   var w=0;
-  for (i=1; i< 6; i++){
-    var x = document.getElementById("motion").rows[i].cells;
+  for (i=1; i< 6; i++){ //for loop that goes through each row of the table, 'i' being used to represent row (should probably change to r)
+    var x = document.getElementById("motion").rows[i].cells; //gets the cells in the row 'i' and sets to x
     var y;
-    x[2].innerHTML =  y;
-    if (y<2) {
+    x[2].innerHTML =  y; //sets a variable y to the value that is currently in the 3rd colunn (the value column)
+    if (y<2) { //if that obtained value is less than 2 (arbitrary number for now), style red and add to counter w which will be used to determine if any row had an error
       x[2].style.backgroundColor = '#FC6962';
       w++;
     }
-    else if (y>90) {
+    else if (y>90) { //if that obtained value is greater than 90 (arbitrary value for now), style green
       x[2].style.backgroundColor = '#C8EEC4';
     }
-    else {
+    else { //if it is in between remain, or change back, to white background
       x[2].style.backgroundColor = '#fff';
     }
   }
-  if (w != 0) {
+  if (w != 0) { //if there was an error in any row during one run of the for loop, meaning w is not 0 as it was created as, then change the class of the div that tavble is in to 'error', which will make the border color red
     document.getElementById("motion_div").className = "error";
     w=0;
   }
-  else {
+  else { //if there was not an error during the for loop in any row, then keep the class of the div as 'ok'
     document.getElementById("motion_div").className = "ok"; 
     w=0;
   }
