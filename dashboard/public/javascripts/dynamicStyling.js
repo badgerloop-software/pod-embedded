2ac01eb777895875dee42f79e0520a3532c7e9c8
Luke Houge <lukehouge@gmail.com>
2018-11-27 15:26:09 -0600

    added comments and updated status code

--- dashboard/public/javascripts/dynamicStyling.js
+++ dashboard/public/javascripts/dynamicStyling.js
@@ -1,18 +1,18 @@
 //random generator for motion
 setInterval(function(){
   var w=0;
-  var table = document.getElementById('motion');
-    for (var r = 1, n = table.rows.length; r < n; r++) {
-      var y = table.rows[r].cells[2].innerHTML;
-      if (y<10) {
-        table.rows[r].cells[2].style.backgroundColor = '#FC6962';
-        w++;
+  var table = document.getElementById('motion'); //creates table array
+    for (var r = 1, n = table.rows.length; r < n; r++) { //iterates through rows in given table
+      var y = table.rows[r].cells[2].innerHTML; //sets the value to y
+      if (y<10) { //checks if too low
+        table.rows[r].cells[2].style.backgroundColor = '#FC6962'; //makes red
+        w++; //adds to w, signifying that there is an error present in the table
       }
-      else if (y>200) {
-        table.rows[r].cells[2].style.backgroundColor = '#C8EEC4';
+      else if (y>200) { //checks if in good range
+        table.rows[r].cells[2].style.backgroundColor = '#C8EEC4'; //makes green
       }
       else {
-        table.rows[r].cells[2].style.backgroundColor = '#fff';
+        table.rows[r].cells[2].style.backgroundColor = '#fff'; //else sets to white background
       }
     }
   if (w != 0) { //if there was an error in any row during one run of the for loop, meaning w is not 0 as it was created as, then change the class of the div that tavble is in to 'error', which will make the border color red
@@ -24,7 +24,7 @@ setInterval(function(){
     w=0;
   }
   //dummy function for status, 2-10= connected, 1= disconected
-  var c =1;
+  var c =2;
   if (c > 1) {
     document.getElementById("motion_status").className = "connected"; 
   }
@@ -59,13 +59,13 @@ setInterval(function(){
     w=0;
   }
   //dummy function for status, 2-10= connected, 1= disconected
-  // var c = 1;
-  // if (c > 1) {
-  //   document.getElementById("breaking_status").className = "connected"; 
-  // }
-  // if (c == 1) {
-    document.getElementById("breaking_status").className = "disconnected"; 
-  // }
+  var c =2;
+  if (c > 1) {
+    document.getElementById("braking_status").className = "connected"; 
+  }
+  if (c == 1) {
+    document.getElementById("braking_status").className = "disconnected"; 
+  }
 }, 100);
 
 //random generator for battery
@@ -93,14 +93,14 @@ setInterval(function(){
     document.getElementById("battery_pack_div").className = "ok"; 
     w=0;
   }
-  //dummy function for status, 2-10= connected, 1= disconected
-  // var c;
-  // if (c > 1) {
-  //   document.getElementById("battery_status").className = "connected"; 
-  // }
-  // if (c == 1) {
-    document.getElementById("battery_status").className = "disconnected"; 
-  // }
+ //dummy function for status, 2-10= connected, 1= disconected
+ var c =2;
+ if (c > 1) {
+   document.getElementById("battery_status").className = "connected"; 
+ }
+ if (c == 1) {
+   document.getElementById("battery_status").className = "disconnected"; 
+ }
 }, 100);
 
 //random generator for motor
@@ -129,11 +129,11 @@ setInterval(function(){
     w=0;
   }
   //dummy function for status, 2-10= connected, 1= disconected
-  // var c = 1;
-  // if (c == 1) {
-  //   document.getElementById("motor_status").className = "connected"; 
-  // }
-  // if (c >1) {
+  var c =1;
+  if (c > 1) {
+    document.getElementById("motor_status").className = "connected"; 
+  }
+  if (c == 1) {
     document.getElementById("motor_status").className = "disconnected"; 
-  // }
+  }
 }, 100);
