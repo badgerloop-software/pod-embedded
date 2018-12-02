943ea9e3b8c49633899769c50add27d5e5b46df1
Luke Houge <lukehouge@gmail.com>
2018-12-02 17:00:01 -0600

    fixed dynamic styling

--- dashboard/public/javascripts/dynamicStyling.js
+++ dashboard/public/javascripts/dynamicStyling.js
@@ -1,22 +1,20 @@
-const UPDATE_RATE = 100;
 //random generator for motion
 setInterval(function(){
   var w=0;
-  for (i=1; i< 6; i++){ //for loop that goes through each row of the table, 'i' being used to represent row (should probably change to r)
-    var x = document.getElementById("motion").rows[i].cells; //gets the cells in the row 'i' and sets to x
-    var y;
-    //sets a variable y to the value that is currently in the 3rd colunn (the value column)
-    if (y<2) { //if that obtained value is less than 2 (arbitrary number for now), style red and add to counter w which will be used to determine if any row had an error
-      x[2].style.backgroundColor = '#FC6962';
-      w++;
+  var table = document.getElementById('motion'); //creates table array
+    for (var r = 1, n = table.rows.length; r < n; r++) { //iterates through rows in given table
+      var y = table.rows[r].cells[2].innerHTML; //sets the value to y
+      if (y<10) { //checks if too low
+        table.rows[r].cells[2].style.backgroundColor = '#FC6962'; //makes red
+        w++; //adds to w, signifying that there is an error present in the table
+      }
+      else if (y>200) { //checks if in good range
+        table.rows[r].cells[2].style.backgroundColor = '#C8EEC4'; //makes green
+      }
+      else {
+        table.rows[r].cells[2].style.backgroundColor = '#fff'; //else sets to white background
+      }
     }
-    else if (y>90) { //if that obtained value is greater than 90 (arbitrary value for now), style green
-      x[2].style.backgroundColor = '#C8EEC4';
-    }
-    else { //if it is in between remain, or change back, to white background
-      x[2].style.backgroundColor = '#fff';
-    }
-  }
   if (w != 0) { //if there was an error in any row during one run of the for loop, meaning w is not 0 as it was created as, then change the class of the div that tavble is in to 'error', which will make the border color red
     document.getElementById("motion_div").className = "error";
     w=0;
@@ -26,32 +24,32 @@ setInterval(function(){
     w=0;
   }
   //dummy function for status, 2-10= connected, 1= disconected
-  // var c =1;
-  // if (c > 1) {
-  //   document.getElementById("motion_status").className = "connected"; 
-  // }
-  // if (c == 1) {
+  var c =2;
+  if (c > 1) {
+    document.getElementById("motion_status").className = "connected"; 
+  }
+  if (c == 1) {
     document.getElementById("motion_status").className = "disconnected"; 
-  // }
-}, UPDATE_INTERVAL);
+  }
+}, 100);
 
 //random generator for braking and pressures
 setInterval(function(){
   var w=0;
-  for (i=1; i< 9; i++){
-    var x = document.getElementById("braking").rows[i].cells;
-    var y;
-    if (y<2) {
-      x[2].style.backgroundColor = '#FC6962';
-      w++;
+  var table = document.getElementById('braking');
+    for (var r = 1, n = table.rows.length; r < n; r++) {
+      var y = table.rows[r].cells[2].innerHTML;
+      if (y<10) {
+        table.rows[r].cells[2].style.backgroundColor = '#FC6962';
+        w++;
+      }
+      else if (y>200) {
+        table.rows[r].cells[2].style.backgroundColor = '#C8EEC4';
+      }
+      else {
+        table.rows[r].cells[2].style.backgroundColor = '#fff';
+      }
     }
-    else if (y>90) {
-      x[2].style.backgroundColor = '#C8EEC4';
-    }
-    else {
-      x[2].style.backgroundColor = '#fff';
-    }
-  }
   if (w != 0) {
     document.getElementById("braking_div").className = "error";
     w=0;
@@ -61,32 +59,32 @@ setInterval(function(){
     w=0;
   }
   //dummy function for status, 2-10= connected, 1= disconected
-  // var c = 1;
-  // if (c > 1) {
-  //   document.getElementById("breaking_status").className = "connected"; 
-  // }
-  // if (c == 1) {
-    document.getElementById("breaking_status").className = "disconnected"; 
-  // }
-}, UPDATE_INTERVAL);
+  var c =2;
+  if (c > 1) {
+    document.getElementById("braking_status").className = "connected"; 
+  }
+  if (c == 1) {
+    document.getElementById("braking_status").className = "disconnected"; 
+  }
+}, 100);
 
 //random generator for battery
 setInterval(function(){
   var w=0;
-  for (i=1; i< 9; i++){
-    var x = document.getElementById("battery_pack").rows[i].cells;
-    var y;
-    if (y<2) {
-      x[2].style.backgroundColor = '#FC6962';
-      w++;
+  var table = document.getElementById('battery_pack');
+    for (var r = 1, n = table.rows.length; r < n; r++) {
+      var y = table.rows[r].cells[2].innerHTML;
+      if (y<10) {
+        table.rows[r].cells[2].style.backgroundColor = '#FC6962';
+        w++;
+      }
+      else if (y>200) {
+        table.rows[r].cells[2].style.backgroundColor = '#C8EEC4';
+      }
+      else {
+        table.rows[r].cells[2].style.backgroundColor = '#fff';
+      }
     }
-    else if (y>90) {
-      x[2].style.backgroundColor = '#C8EEC4';
-    }
-    else {
-      x[2].style.backgroundColor = '#fff';
-    }
-  }
   if (w != 0) {
     document.getElementById("battery_pack_div").className = "error";
     w=0;
@@ -95,33 +93,33 @@ setInterval(function(){
     document.getElementById("battery_pack_div").className = "ok"; 
     w=0;
   }
-  //dummy function for status, 2-10= connected, 1= disconected
-  // var c;
-  // if (c > 1) {
-  //   document.getElementById("battery_status").className = "connected"; 
-  // }
-  // if (c == 1) {
-    document.getElementById("battery_status").className = "disconnected"; 
-  // }
-}, UPDATE_INTERVAL);
+ //dummy function for status, 2-10= connected, 1= disconected
+ var c =2;
+ if (c > 1) {
+   document.getElementById("battery_status").className = "connected"; 
+ }
+ if (c == 1) {
+   document.getElementById("battery_status").className = "disconnected"; 
+ }
+}, 100);
 
 //random generator for motor
 setInterval(function(){
   var w=0;
-  for (i=1; i< 5; i++){
-    var x = document.getElementById("motor").rows[i].cells;
-    var y;
-    if (y<2) {
-      x[2].style.backgroundColor = '#FC6962';
-      w++;
+  var table = document.getElementById('motor');
+    for (var r = 1, n = table.rows.length; r < n; r++) {
+      var y = table.rows[r].cells[2].innerHTML;
+      if (y<10) {
+        table.rows[r].cells[2].style.backgroundColor = '#FC6962';
+        w++;
+      }
+      else if (y>200) {
+        table.rows[r].cells[2].style.backgroundColor = '#C8EEC4';
+      }
+      else {
+        table.rows[r].cells[2].style.backgroundColor = '#fff';
+      }
     }
-    else if (y>90) {
-      x[2].style.backgroundColor = '#C8EEC4';
-    }
-    else {
-      x[2].style.backgroundColor = '#fff';
-    }
-  }
   if (w != 0) {
     document.getElementById("motor_div").className = "error";
     w=0;
@@ -131,11 +129,11 @@ setInterval(function(){
     w=0;
   }
   //dummy function for status, 2-10= connected, 1= disconected
-  // var c = 1;
-  // if (c == 1) {
-  //   document.getElementById("motor_status").className = "connected"; 
-  // }
-  // if (c >1) {
+  var c =1;
+  if (c > 1) {
+    document.getElementById("motor_status").className = "connected"; 
+  }
+  if (c == 1) {
     document.getElementById("motor_status").className = "disconnected"; 
-  // }
-}, UPDATE_INTERVAL);
+  }
+}, 100);
