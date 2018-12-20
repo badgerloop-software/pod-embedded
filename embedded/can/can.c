dab9ff1ed44bc795cdfee2fbeee2fb181afcfe02
Ezra Boley <eboley@wisc.edu>
2018-12-20 16:31:09 -0600

    added tx functionality. Should be able to send CAN messages and the API is set up. Maybe a little rough around the edges but overall clean, minimal, and functioning

--- embedded/can/can.c
+++ embedded/can/can.c
@@ -1,6 +1,7 @@
 #include <linux/can.h>
 #include <net/if.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include "can.h"
 #include <unistd.h>
 //#include <linux/interrupt.h>
@@ -13,8 +14,8 @@
 
 #define CAN_INTF "vcan0"
 
-struct sockaddr_can addr;
-struct ifreq ifr;  // Used to look at flags on the network interface
+static struct sockaddr_can addr;
+static struct ifreq ifr;  // Used to look at flags on the network interface
 
 volatile bool NEW_CAN_MESSAGE = false;
 
@@ -23,7 +24,6 @@ static const struct itimerval new_val = {
     {0, 10000}, 
     {0, 10000}
 };
-
 
 
 void can_rx_irq(){   
@@ -59,15 +59,38 @@ int init_can_timer(const struct itimerval *new, struct itimerval *old) {
 
 inline int read_can_message(struct can_frame *recvd_msg) {
     int nBytes = recv(can_sock, recvd_msg, sizeof(struct can_frame), MSG_DONTWAIT);
+    /* This is actually ok if it fails here, it just means no new info */
     if (nBytes < 0) {
-        return -1;
+        return 1;
     }
     return 0;
 }
 
 
-bool init_can() {
-    init_can_connection(&can_sock);
-    init_can_timer(&new_val, NULL);
-    return true;
+inline int send_can_msg(uint32_t id, uint8_t *data, uint8_t size) {
+    struct can_frame tx_msg;
+
+    tx_msg.can_dlc = size;
+    tx_msg.can_id = id;     // Should actually be 11 bits max
+    
+    for(int i = 0; i < size; i++) {
+        tx_msg.data[i] = data[i];
+    }
+    
+    send(can_sock, &tx_msg, sizeof(struct can_frame), MSG_DONTWAIT);
+
+    return 0;   // Not much we do with error codes here
+}
+
+
+int init_can() {
+    if (init_can_connection(&can_sock)) {
+        fprintf(stderr, "Failed to init\n\r");
+        return 1;
+    }
+    if (init_can_timer(&new_val, NULL)) {
+        fprintf(stderr, "Failed to set read timer\n\r");
+        return 1;
+    }
+    return 0;
 }
--- embedded/can/can_test.c
+++ embedded/can/can_test.c
@@ -4,13 +4,16 @@
 int main() {
     init_can();
     struct can_frame can_mesg;
+    uint8_t data[4] = {0xDE, 0xAD, 0xBE, 0xEF};
     while (1) {
-        if (NEW_CAN_MESSAGE) {
+/*        if (NEW_CAN_MESSAGE) {
             read_can_message(&can_mesg);
-            printf("CAN data: [%#X , %#X , %#X , %#X , %#X , %#X , %#X , %#X]\n\r", can_mesg.data[0], can_mesg.data[1], can_mesg.data[2], can_mesg.data[3], can_mesg.data[4], can_mesg.data[5], can_mesg.data[6], can_mesg.data[7]);
+            printf("ID: %#X || ", can_mesg.can_id);
+            printf("Data: [%#X.%#X.%#X.%#X.%#X.%#X.%#X.%#X]\n\r", can_mesg.data[0], can_mesg.data[1], can_mesg.data[2], can_mesg.data[3], can_mesg.data[4], can_mesg.data[5], can_mesg.data[6], can_mesg.data[7]);
             NEW_CAN_MESSAGE = false;
-        }
-        usleep(100000);
+        }*/
+        send_can_msg(0x123, data, 4);
+        usleep(500000);
     }
     return 0;
 }
