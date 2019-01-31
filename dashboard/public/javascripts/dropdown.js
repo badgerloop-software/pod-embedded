9bf4eaa08382fb69382e6fb65f3688ea1cb2c846
Eric <coolspot1000@gmail.com>
2019-01-30 18:05:40 -0600

    Condensed filterFunction

--- dashboard/public/javascripts/dropdown.js
+++ dashboard/public/javascripts/dropdown.js
@@ -15,24 +15,10 @@ function dropdown(num) {
 }
 
 function filterFunction(a) {
-  var inputnum;
-  var dropnum;
-  if (a==1){
-    inputnum = "dropdownInput1";
-    dropnum = "myDropdown1";
-  }
-  else if (a==2){
-    inputnum = "dropdownInput2";
-    dropnum = "myDropdown2";
-  }
-  else if (a==3){
-    inputnum = "dropdownInput3";
-    dropnum = "myDropdown3";
-  }
   var input, filter, ul, li, a, i;
-  input = document.getElementById(inputnum);
+  input = document.getElementById("dropdownInput"+String(a));
   filter = input.value.toUpperCase();
-  div = document.getElementById(dropnum);
+  div = document.getElementById("myDropdown"+String(a));
   a = div.getElementsByTagName("a");
   for (i = 0; i < a.length; i++) {
       if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
