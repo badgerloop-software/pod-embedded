796bc608241c500479f43d85937e6c0f1591974d
Eric <coolspot1000@gmail.com>
2019-01-30 18:06:45 -0600

    Condensed Dropdown

--- dashboard/public/javascripts/dropdown.js
+++ dashboard/public/javascripts/dropdown.js
@@ -1,17 +1,9 @@
-/*
+n/*
 Author: Luke Houge
 Purpose: Enable dynamic and searchable dropdowns
 */
 function dropdown(num) {
-  if (num==1){
-    document.getElementById("myDropdown1").classList.toggle("show");
-  }
-  else if (num ==2){
-    document.getElementById("myDropdown2").classList.toggle("show");
-  }
-  else if (num ==3){
-    document.getElementById("myDropdown3").classList.toggle("show");
-  }
+  document.getElementById("myDropdown"+String(num)).classList.toggle("show");
 }
 
 function filterFunction(a) {
