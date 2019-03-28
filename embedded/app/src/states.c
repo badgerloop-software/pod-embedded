e30c957ad51229e0e93fe613907d5d71bab560f2
rdaruwala <rdaruwala@gmail.com>
2019-03-28 15:44:31 -0500

    Update braking values and move to new file

--- embedded/app/src/states.c
+++ embedded/app/src/states.c
@@ -14,31 +14,7 @@
 
 #include "state_machine.h"
 #include "data.h"
-
-
-/* Defines */
-
-/* Pressure sensor acceptable limits (in PSI) */
-#define PS1_BOTTOM_LIMIT        1000
-#define PS1_TOP_LIMIT           3000
-#define PS2_BOTTOM_LIMIT        1000
-#define PS2_TOP_LIMIT           3000
-#define PS3_BOTTOM_LIMIT        1000
-#define PS3_TOP_LIMIT           3000
-#define PS4_BOTTOM_LIMIT        0
-#define PS4_TOP_LIMIT           20
-/**/
-
-#define MAX_BATT_TEMP			60	/* Degrees Celcius */
-#define MAX_STOPPED_ACCEL		0.3 /* Limit for qualifying the pod as stopped in m/s */
-
-/* In mV */
-#define MAX_CELL_VOLTAGE        4200    
-#define MIN_CELL_VOLTAGE        3000
-
-#define TUBE_LENGTH             3800    /*in ft //FIXME I could be wrong on this on haha */
-
-#define MAX_RUN_TIME            30  /* in seconds, TODO get the real number */
+#include "states.h"
 
 /* Imports/Externs */
 
@@ -163,7 +139,9 @@ stateTransition_t * propulsionAction() {
     // Check for nominal values?
     
 
-    if (data->timers->global > MAX_RUN_TIME
+    if (data->timers->global > MAX_RUN_TIME){
+        
+    }
 
     return NULL;
 }
