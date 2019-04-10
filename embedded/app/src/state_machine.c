c451889650d5212fcf17751507df896f00f786bc
Ezra Boley <eboley@wisc.edu>
2019-04-10 00:46:59 -0500

    Adding transition initialization (minus the functions for each) and having them added to their appropriate state

--- embedded/app/src/state_machine.c
+++ embedded/app/src/state_machine.c
@@ -116,37 +116,25 @@ void runStateMachine(void) {
  */
 void buildStateMachine(void) {
 	    /* Create all of the states*/
-	state_t *powerOn, *idle, *readyForPumpdown, *pumpdown,
+	state_t *powerOff, *idle, *readyForPumpdown, *pumpdown,
 			*readyForLaunch, *propulsion, *braking, *secondaryBraking,
 			*stopped, *crawl, *rebrake, *postRun, *safeToApproach, *preFault,
 			*runFault, *postFault;
 	state_t **allStates = malloc(NUM_STATES * sizeof(state_t*));
+    
+	stateMachine.allStates = 
+    {
+            powerOff, idle, readyForPumpdown, pumpdown,
+            readyForLaunch, propulsion, braking, secondaryBraking,
+            stopped, crawl, postRun, safeToApproach, preFault, runFault, postFault
+    };
+    
+    // This is the new API
+    initPowerOff(powerOff);
+    initIdle(idle);
+    initReadyForPumpdown(readyForPumpdown);
 
-	stateMachine.allStates = allStates;
-
-    /* Allocating space for each state */
-	if ((powerOn = malloc(sizeof(state_t)))           == NULL) { STATE_ERROR(); }
-	if ((idle = malloc(sizeof(state_t)))              == NULL) { STATE_ERROR(); }
-	if ((readyForPumpdown = malloc(sizeof(state_t)))  == NULL) { STATE_ERROR(); }
-	if ((pumpdown = malloc(sizeof(state_t)))          == NULL) { STATE_ERROR(); }
-	if ((readyForLaunch = malloc(sizeof(state_t)))    == NULL) { STATE_ERROR(); }
-	if ((propulsion = malloc(sizeof(state_t)))        == NULL) { STATE_ERROR(); }
-	if ((braking = malloc(sizeof(state_t)))           == NULL) { STATE_ERROR(); }
-	if ((secondaryBraking = malloc(sizeof(state_t)))  == NULL) { STATE_ERROR(); }
-	if ((stopped = malloc(sizeof(state_t)))           == NULL) { STATE_ERROR(); }
-	if ((crawl = malloc(sizeof(state_t)))             == NULL) { STATE_ERROR(); }
-	if ((rebrake = malloc(sizeof(state_t)))           == NULL) { STATE_ERROR(); }
-	if ((postRun = malloc(sizeof(state_t)))           == NULL) { STATE_ERROR(); }
-	if ((safeToApproach = malloc(sizeof(state_t)))    == NULL) { STATE_ERROR(); }
-	if ((preFault = malloc(sizeof(state_t)))          == NULL) { STATE_ERROR(); }
-	if ((runFault = malloc(sizeof(state_t)))          == NULL) { STATE_ERROR(); }
-	if ((postFault = malloc(sizeof(state_t)))         == NULL) { STATE_ERROR(); }
-
-    /* Initializing states TODO add the num transition arguement to each of
-     * these*/
-	initState(powerOn, POWER_OFF_NAME, powerOnAction, 1);   // FIXME Not sure if this state should be poweroff or poweron
-	initState(idle, IDLE_NAME, idleAction, 1);
-	initState(readyForPumpdown, READY_FOR_PUMPDOWN_NAME, readyForPumpdownAction, 1);
+    // TODO replace old way of creating states
 	initState(pumpdown, PUMPDOWN_NAME, pumpdownAction, 1);
 	initState(readyForLaunch, READY_NAME, readyForLaunchAction, 1);
 	initState(propulsion, PROPULSION_NAME, propulsionAction, 1);
@@ -158,11 +146,6 @@ void buildStateMachine(void) {
 	initState(preFault, PRE_RUN_FAULT_NAME, preFaultAction, 1);
 	initState(runFault, RUN_FAULT_NAME, runFaultAction, 1);
 	initState(postFault, POST_RUN_FAULT_NAME, postFaultAction, 1);
-
-    /* Create all of the transitions */
-    
-    
-
 }
 
 /***
@@ -175,8 +158,12 @@ void buildStateMachine(void) {
  */
 static void initState(state_t* state, char* name, stateTransition_t *(*action)(), int numTransitions ) {
     static int indexInAllStates = 0;
-    
     int i = 0;
+    
+    state = malloc(sizeof(state_t));
+    if (state == NULL) {
+        STATE_ERROR();
+    }
     strncpy(state->name, name, strlen(name) );
     state->action = action;
     state->numTransitions = numTransitions;
@@ -194,6 +181,8 @@ static void initState(state_t* state, char* name, stateTransition_t *(*action)()
  *
  */
 static void initTransition(stateTransition_t *transition, state_t *target, bool (*action)() ) {
+    transition = malloc(sizeof(stateTransition_t));
+    if (transition == NULL) return -1;
     transition->target;
 }
 
@@ -207,6 +196,38 @@ static int addTransition(state_t *state, stateTransition_t *trans) {
     state->transitions[i] = trans;
 }
 
-static int initPowerOff() {
+static int initPowerOff(state_t *powerOff) {
+    stateTransition_t *toIdle, *toPreFault;
+	
+    initState(powerOff, POWER_OFF_NAME, powerOnAction, 2);   
     
+    initTransition(toIdle, findState(IDLE_NAME), NULL);
+    initTransition(toPreFault, findState(PRE_RUN_FAULT_NAME), NULL); 
+    addTransition(powerOff, toIdle);
+    addTransition(powerOff, toPreFault);
+    return 0;
+}
+
+static int initIdle(state_t *idle) {
+    stateTransition_t *toReadyForPumpdown, *toPreFault;
+
+    initState(idle, IDLE_NAME, idleAction, 2);
+
+    initTransition(toReadyForPumpdown, findState(READY_FOR_PUMPDOWN_NAME), NULL);
+    initTransition(toPreFault, findState(PRE_RUN_FAULT_NAME), NULL);
+    addTransition(idle, toReadyForPumpdown);
+    addTransition(idle, toPreFault);
+    return 0;
+}
+
+static int initReadyForPumpdown(state_t *readyForPumpdown) {
+    stateTransition_t *toPumpdown, *toPreFault;
+
+    initState(readyForPumpdown, READY_FOR_PUMPDOWN_NAME, readyForPumpdownAction, 2);
+    
+    initTransition(toPumpdown, findState(PUMPDOWN_NAME), NULL);
+    initTransition(toPreFault, findStaet(PRE_RUN_FAULT_NAME), NULL);
+    addTransition(readyForPumpdown, toPumpdown);
+    addTransition(readyForPumpdown, toPreFault);
+    return 0;
 }
