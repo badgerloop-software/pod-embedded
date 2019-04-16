301eb5398bc1d3484e088dc1df8dba7539482dca
rdaruwala <rdaruwala@gmail.com>
2019-04-15 20:31:47 -0500

    stateMachines bug fixes 1

--- embedded/app/src/state_machine.c
+++ embedded/app/src/state_machine.c
@@ -43,121 +43,6 @@ extern stateTransition_t * postFaultAction(void);
 stateMachine_t stateMachine;
 
 /***
- * findState - Searches all the created states and returns the one with a matching name
- *
- * ARGS: char *stateName - Name of the state we are searching for. Check out state_machine.h
- * 	for options.
- *
- * RETURNS: state_t, the found state or NULL if that state doesnt exist
- */
-state_t *findState(char *stateName) {
-    for (int i = 0; i < NUM_STATES; i++) {
-        if (strcmp(stateMachine.allStates[i]->name, stateName) == 0) {
-            return stateMachine.allStates[i];
-        }
-    }
-    return NULL;
-}
-
-/***
- * findTransition - Looks through a passed in states list of transitions
- * 	and identifies the one that leads to a specified target
- *
- * ARGS: state_t srcState - The state whose transitions we are searching through
- * 		 char *targName	  - The name of the state we want a transition to
- *
- * RETURNS: stateTransition_t the transition to the state we want, or NULL
- * 	if no such transition exists.
- */
-stateTransition_t *findTransition(state_t *srcState, char *targName) {
-    for (int i = 0; i < srcState->numTransitions; i++) {
-        if (strcmp(srcState->transitions[i]->target->name, targName) == 0)
-            return srcState->transitions[i];
-    }
-    return NULL;
-}
-
-/***
- * runStateMachine -
- *		Executes the current states action. A mini control loop
- * 	that will be called on every iteration of the overarching control loop. It
- *  will be able to then check if the current state should transition, and if so
- *  it should be able to execute that transition action as well and change the
- *  current state.
- *		It also is responsible for dealing with incoming state commands
- *  from the dashboard, after they have been parsed and appropriate flags have been
- *  set elsewhere.
- *
- */
-void runStateMachine(void) {
-    /* The cmd receiver will populate this field if we get an override */
-    if (stateMachine.overrideStateName != NULL) {
-        state_t *tempState = findState(stateMachine.overrideStateName);
-        /* TODO We also need to execute a transition if it exists here */
-        if (tempState != NULL) {
-            stateTransition_t *trans = findTransition(tempState, stateMachine.overrideStateName);
-            if (trans != NULL) trans->action();
-            stateMachine.currState = tempState;
-        }
-        stateMachine.overrideStateName = NULL;
-    }
-
-    /* execute the state and check if we should be transitioning */
-	stateTransition_t *transition = stateMachine.currState->action();
-    if (transition != NULL) {
-		transition->action();
-		stateMachine.currState = transition->target;
-	}
-}
-
-/***
- * buildStateMachine - allocates memory for each state and creates the
- * 	state machine struct that will contain the meta data for the whole
- *  state machine. Its a globally available because it will be so commonly
- *  used.
- *
- */
-void buildStateMachine(void) {
-	    /* Create all of the states*/
-	state_t *powerOff, *idle, *readyForPumpdown, *pumpdown,
-			*readyForLaunch, *propulsion, *braking, *secondaryBraking,
-			*stopped, *crawl, *rebrake, *postRun, *safeToApproach, *preFault,
-			*runFault, *postFault;
-	state_t **allStates = malloc(NUM_STATES * sizeof(state_t*));
-    
-	stateMachine.allStates = 
-    {
-            powerOff, idle, readyForPumpdown, pumpdown,
-            readyForLaunch, propulsion, braking, secondaryBraking,
-            stopped, crawl, postRun, safeToApproach, preFault, runFault, postFault
-    };
-    
-    initPowerOff(powerOff);
-    initIdle(idle);
-    initReadyForPumpdown(readyForPumpdown);
-    initPumpdown(pumpdown);
-    initReadyForLaunch(readyForLaunch);
-	initPropulsion(propulsion);
-    initBraking(braking);
-    initStopped(stopped);
-    initCrawl(crawl);
-    initPostRun(postRun);
-    initSafeToApproach(safeToApproach);
-    initPreFault(preFault);
-    initRunFault(runFault);
-    initPostFault(postFault);
-
-    stateMachine.currState = powerOff;
-    
-    stateMachine.overrideStateName = malloc(21) // Longest state name is "readyForPropulsion" -- 18 char
-    
-    if(stateMachine.overrideStateName == NULL){
-        fprintf(stderr, "Malloc error -- state machine override state machine name\n");
-        exit(1);
-    }
-}
-
-/***
  * initState - fills in the fields of the state_t struct.
  *
  * ARGS: state_t *state - allocated state (malloced most likely),
@@ -361,3 +246,120 @@ static int initPostFault(state_t *postFault) {
     initState(postFault, POST_RUN_FAULT_NAME, postFaultAction, 0);
     return 0;
 }
+
+
+/***
+ * findState - Searches all the created states and returns the one with a matching name
+ *
+ * ARGS: char *stateName - Name of the state we are searching for. Check out state_machine.h
+ * 	for options.
+ *
+ * RETURNS: state_t, the found state or NULL if that state doesnt exist
+ */
+state_t *findState(char *stateName) {
+    for (int i = 0; i < NUM_STATES; i++) {
+        if (strcmp(stateMachine.allStates[i]->name, stateName) == 0) {
+            return stateMachine.allStates[i];
+        }
+    }
+    return NULL;
+}
+
+/***
+ * findTransition - Looks through a passed in states list of transitions
+ * 	and identifies the one that leads to a specified target
+ *
+ * ARGS: state_t srcState - The state whose transitions we are searching through
+ * 		 char *targName	  - The name of the state we want a transition to
+ *
+ * RETURNS: stateTransition_t the transition to the state we want, or NULL
+ * 	if no such transition exists.
+ */
+stateTransition_t *findTransition(state_t *srcState, char *targName) {
+    for (int i = 0; i < srcState->numTransitions; i++) {
+        if (strcmp(srcState->transitions[i]->target->name, targName) == 0)
+            return srcState->transitions[i];
+    }
+    return NULL;
+}
+
+/***
+ * runStateMachine -
+ *		Executes the current states action. A mini control loop
+ * 	that will be called on every iteration of the overarching control loop. It
+ *  will be able to then check if the current state should transition, and if so
+ *  it should be able to execute that transition action as well and change the
+ *  current state.
+ *		It also is responsible for dealing with incoming state commands
+ *  from the dashboard, after they have been parsed and appropriate flags have been
+ *  set elsewhere.
+ *
+ */
+void runStateMachine(void) {
+    /* The cmd receiver will populate this field if we get an override */
+    if (stateMachine.overrideStateName != NULL) {
+        state_t *tempState = findState(stateMachine.overrideStateName);
+        /* TODO We also need to execute a transition if it exists here */
+        if (tempState != NULL) {
+            stateTransition_t *trans = findTransition(tempState, stateMachine.overrideStateName);
+            if (trans != NULL) trans->action();
+            stateMachine.currState = tempState;
+        }
+        stateMachine.overrideStateName = NULL;
+    }
+
+    /* execute the state and check if we should be transitioning */
+	stateTransition_t *transition = stateMachine.currState->action();
+    if (transition != NULL) {
+		transition->action();
+		stateMachine.currState = transition->target;
+	}
+}
+
+/***
+ * buildStateMachine - allocates memory for each state and creates the
+ * 	state machine struct that will contain the meta data for the whole
+ *  state machine. Its a globally available because it will be so commonly
+ *  used.
+ *
+ */
+void buildStateMachine(void) {
+	    /* Create all of the states*/
+	state_t *powerOff, *idle, *readyForPumpdown, *pumpdown,
+			*readyForLaunch, *propulsion, *braking, *secondaryBraking,
+			*stopped, *crawl, *rebrake, *postRun, *safeToApproach, *preFault,
+			*runFault, *postFault;
+	state_t **allStates = malloc(NUM_STATES * sizeof(state_t*));
+    
+	stateMachine.allStates = 
+    {
+            powerOff, idle, readyForPumpdown, pumpdown,
+            readyForLaunch, propulsion, braking, secondaryBraking,
+            stopped, crawl, postRun, safeToApproach, preFault, runFault, postFault
+    };
+    
+    initPowerOff(powerOff);
+    initIdle(idle);
+    initReadyForPumpdown(readyForPumpdown);
+    initPumpdown(pumpdown);
+    initReadyForLaunch(readyForLaunch);
+	initPropulsion(propulsion);
+    initBraking(braking);
+    initStopped(stopped);
+    initCrawl(crawl);
+    initPostRun(postRun);
+    initSafeToApproach(safeToApproach);
+    initPreFault(preFault);
+    initRunFault(runFault);
+    initPostFault(postFault);
+
+    stateMachine.currState = powerOff;
+    
+    stateMachine.overrideStateName = malloc(21); // Longest state name is "readyForPropulsion" -- 18 char
+    
+    if(stateMachine.overrideStateName == NULL){
+        fprintf(stderr, "Malloc error -- state machine override state machine name\n");
+        exit(1);
+    }
+}
+
