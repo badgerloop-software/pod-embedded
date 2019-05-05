99479f1a0dd8677ed9c9e7ca888a8865c29ba320
Ezra Boley <eboley@wisc.edu>
2019-05-05 18:45:34 +0000

    Created an API for the BB Gpio pins, should allow any program to interact with them via their sysfs interface

--- embedded/drivers/include/bbgpio.h
+++ embedded/drivers/include/bbgpio.h
@@ -1,7 +1,32 @@
 #ifndef __BBGPIO_H__
 #define __BBGPIO_H__
 
-int bbGpioFdOpen(int gpio);
+#include <stdbool.h>
+
+#define RISING_EDGE  "rising"
+#define FALLING_EDGE "falling"
+#define BOTH_EDGE	 "both"
+
+#define DIR_OUT		 "out"
+#define DIR_IN		 "in"
+
+int bbGetAbsPinNum(unsigned int bank, unsigned int num);
+
+int bbGpioExport(unsigned int gpio);
+
+int bbGpioSetEdge(unsigned int gpio, char * edge);
+
+int bbGpioSetDir(unsigned int gpio, char * dir);
+
+int bbGpioSetValue(unsigned int gpio, bool val);
+
+char * bbGpioGetEdge(unsigned int gpio);
+
+char * bbGpioGetDir(unsigned int gpio);
+
+bool bbGpioGetValue(unsigned int gpio);
+
+int bbGpioFdOpen(unsigned int gpio);
 
 int bbGpioFdClose(int fd);
 
