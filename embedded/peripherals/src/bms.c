c3af8906923dcbb90afd0f0085cd1997f0e74e45
Ezra Boley <eboley@wisc.edu>
2019-05-30 00:20:08 -0500

    Added some useful debug defines that can be conditionally included at compile time. Also added rough voting, enough to test

--- embedded/peripherals/src/bms.c
+++ embedded/peripherals/src/bms.c
@@ -30,33 +30,37 @@ int bmsClearFaults(void){
   * Receives a CAN Message and updates global BMS_Data struct
   */
 int bmsParseMsg(uint32_t id, uint8_t *msg) {
-	//printf("Recieved bms message\r\n");
-	bms_t *bms = data->bms;
+#ifdef DEBUG_BMS
+	printf("Recieved bms message\n");
+    printf("ID: 0x%3lx\r\n", (long unsigned int) id);
+    printf("Data: %d, %d, %d, %d, %d, %d, %d\n", msg[0], msg[1], msg[2], 
+	    msg[3], msg[4], msg[5], msg[6]);
+#endif
+    bms_t *bms = data->bms;
 	switch(id) {
-		case 0x6B0:
-//			printf("ID: 0x%3lx\r\n", (long unsigned int) id);
-//			printf("Data: %d, %d, %d, %d, %d, %d, %d\r\n", msg[0], msg[1], msg[2], 
-//			msg[3], msg[4], msg[5], msg[6]);
-			bms->packCurrent = msg[1] | msg[0] << 8;
+		case 0x6B0:		
+            bms->packCurrent = msg[1] | msg[0] << 8;
 			bms->packCurrent /= 10;
 			bms->packVoltage = msg[3] | msg[2] << 8;
 			bms->packVoltage /= 10;
 			bms->Soc = msg[4]/2;
 			bms->relayStatus = msg[6] | msg[5] << 8;
-
-//			printf("V: %f\r\n", bms->packVoltage);
-//			printf("A: %f\r\n", bms->packCurrent);
-//			printf("Soc: %d\r\n", bms->Soc);
-//			printf("Relay: %d\r\n", bms->relayStatus);
-			break;
+#ifdef DEBUG_BMS
+			printf("V: %f\r\n", bms->packVoltage);
+			printf("A: %f\r\n", bms->packCurrent);
+			printf("Soc: %d\r\n", bms->Soc);
+			printf("Relay: %d\r\n", bms->relayStatus);
+#endif
+            break;
 		case 0x6B1:
 			bms->packDCL = msg[1] | msg[0] << 8;
 			bms->highTemp = msg[4];
 			bms->lowTemp = msg[5];
-//			printf("DCL: %d\r\n", bms->packDCL);
-//			printf("High T: %d\r\n", bms->highTemp);
-//			printf("Low T: %d\r\n", bms->lowTemp);
-
+#ifdef DEBUG_BMS
+            printf("DCL: %d\r\n", bms->packDCL);
+			printf("High T: %d\r\n", bms->highTemp);
+			printf("Low T: %d\r\n", bms->lowTemp);
+#endif
 			break;
 		case 0x653:
 			printf("ID: 0x%3lx\r\n", (long unsigned int) id);
@@ -64,9 +68,11 @@ int bmsParseMsg(uint32_t id, uint8_t *msg) {
 			bms->relayStatus = msg[0];
 			bms->inputVoltage = msg[2] | (msg[3] << 8);
 			bms->inputVoltage /= 10;
-//			printf("Relay status %d\r\n", bms->relayStatus);
-//			printf("Input Source Supply Voltage: %f\r\n", bms->inputVoltage);
-			break;
+#ifdef DEBUG_BMS
+            printf("Relay status %d\r\n", bms->relayStatus);
+			printf("Input Source Supply Voltage: %f\r\n", bms->inputVoltage);
+#endif
+            break;
 		case 0x652:
 
 			bms->packCCL = msg[0] | (msg[1] << 8);
@@ -75,9 +81,11 @@ int bmsParseMsg(uint32_t id, uint8_t *msg) {
 			bms->cellMaxVoltage /= 10000;
 			bms->cellMinVoltage = msg[6] | (msg[7] << 8);
 			bms->cellMinVoltage /= 10000;
-//			printf("DCL %d\r\n", bms->packDCL);
-//			printf("Cell Min V:  %d, Cell Max V: %d\r\n", bms->cellMinVoltage, bms->cellMaxVoltage);
-			break;
+#ifdef DEBUG_BMS
+            printf("DCL %d\r\n", bms->packDCL);
+			printf("Cell Min V:  %d, Cell Max V: %d\r\n", bms->cellMinVoltage, bms->cellMaxVoltage);
+#endif
+            break;
 		case 0x651:
 			bms->cellMaxVoltage = msg[2] | (msg[3] << 8);
 			bms->cellMinVoltage = msg[0] | (msg[1] << 8);
@@ -87,11 +95,12 @@ int bmsParseMsg(uint32_t id, uint8_t *msg) {
 			bms->cellMinVoltage /= 1000;
 			bms->maxCells = msg[6];
 			bms->numCells = msg[7];
-
-//			printf("Num Cells %d\r\n", bms->numCells);
-//			printf("Cell Avg V:  %d\r\n", bms->cellAvgVoltage);
-//			printf("Cell Min V:  %d, Cell Max V: %d\r\n", bms->cellMinVoltage, bms->cellMaxVoltage);
-			break;
+#ifdef DEBUG_BMS
+			printf("Num Cells %d\r\n", bms->numCells);
+			printf("Cell Avg V:  %d\r\n", bms->cellAvgVoltage);
+			printf("Cell Min V:  %d, Cell Max V: %d\r\n", bms->cellMinVoltage, bms->cellMaxVoltage);
+#endif
+            break;
 		case 0x650:
 
 			bms->Soc = msg[0];
@@ -101,13 +110,14 @@ int bmsParseMsg(uint32_t id, uint8_t *msg) {
 			bms->packOpenVoltage = msg[4] | (msg[5] << 8);
 			bms->packOpenVoltage /= 10;
 			bms->packCycles = msg[6] | (msg[7] << 8);
-
-//			printf("SOC %d\r\n", bms->Soc);
-//			printf("Pack Resistance %d\r\n", bms->packResistance);
-//			printf("Pack Health %d\r\n", bms->packHealth);
-//			printf("Pack Open Voltage  %f\r\n", bms->packOpenVoltage);
-//			printf("Pack Cycles  %d\r\n", bms->packCycles);
-			break;
+#ifdef DEBUG_BMS
+			printf("SOC %d\r\n", bms->Soc);
+			printf("Pack Resistance %d\r\n", bms->packResistance);
+			printf("Pack Health %d\r\n", bms->packHealth);
+			printf("Pack Open Voltage  %f\r\n", bms->packOpenVoltage);
+            printf("Pack Cycles  %d\r\n", bms->packCycles);
+#endif
+            break;
 		case 0x150:
 
 			bms->packCurrent = msg[0] | (msg[1] << 8);
@@ -117,17 +127,17 @@ int bmsParseMsg(uint32_t id, uint8_t *msg) {
 			bms->packAh = msg[4] | (msg[5] << 8);
 			bms->highTemp = msg[6];
 			bms->lowTemp = msg[7];
-
-//			printf("Pack Current %f\r\n", bms->packCurrent);
-//			printf("Pack Voltage  %f\r\n", bms->packVoltage);
-//			printf("Pack Amp Hours  %d\r\n", bms->packAh);
-//			printf("High Temp  %d\r\n", bms->highTemp);
-//			printf("Low Temp  %d\r\n", bms->lowTemp);
-			break;
+#ifdef DEBUG_BMS
+			printf("Pack Current %f\r\n", bms->packCurrent);
+			printf("Pack Voltage  %f\r\n", bms->packVoltage);
+			printf("Pack Amp Hours  %d\r\n", bms->packAh);
+			printf("High Temp  %d\r\n", bms->highTemp);
+            printf("Low Temp  %d\r\n", bms->lowTemp);
+#endif
+            break;
 		case 0x80:
 			break;
 		default:
-//			printf("BMS CAN command not found 0x%08lx\r\n", (long unsigned int) id);
 			return 0;
 	}
 	return 1;
