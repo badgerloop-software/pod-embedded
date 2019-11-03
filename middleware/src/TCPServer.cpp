18c8d5732453647b857dfe95fa28bf7de507693f
Daniel Kouchekinia <kouchekinia@wisc.edu>
2019-11-02 23:56:47 -0500

    Added in all of the commands from the HV and LV tcp sockets. Tidied some code up.

--- middleware/src/TCPServer.cpp
+++ middleware/src/TCPServer.cpp
@@ -2,15 +2,25 @@
 #include <vector>
 #include <iostream>
 #include <string.h>
-#include <cctype>
 #include <future>
-#include <algorithm>
 #include <PracticalSocket/PracticalSocket.h>
+
+extern "C" {
+    #include "data.h"
+    #include "state_machine.h"
+    #include "hv_iox.h"
+    #include "braking.h"
+    #include "motor.h"
+    #include "rms.h"
+
+    extern void resetNav();
+    extern stateMachine_t stateMachine;
+}
 
 pthread_t TCPThread;
 
 void SetupTCPServer() {
-    if (pthread_create(&TCPThread, NULL, TCPLoop, NULL))
+    if (pthread_create(&TCPThread, nullptr, TCPLoop, nullptr))
         fprintf(stderr, "Error creating TCP loop thread\n");
 }
 
@@ -18,7 +28,7 @@ void *TCPLoop(void *arg) {
 
     TCPServerSocket serverSocket(TCP_PORT);
 
-    while(true){
+    while(1){
         TCPSocket* socket = serverSocket.accept();
 
         pthread_t clientThread;
@@ -31,7 +41,7 @@ void *HandleConnection(void *arg) {
     // Detach the current thread to deallocate resources on return
     pthread_detach(pthread_self());
 
-    TCPSocket* socket = static_cast<TCPSocket*>(arg);
+    auto* socket = static_cast<TCPSocket*>(arg);
 
     cout << "TCP connection CREATED with " << socket -> getLocalAddress() << ":" << socket -> getLocalPort() << ".\n";
     cout.flush();
@@ -89,6 +99,132 @@ void *HandleConnection(void *arg) {
         if(strncmp(cmd, "keepalive", cmdLength) == 0)
             continue;
 
+        /**
+         ** POD COMMANDS GO HERE
+        **/
+
+        // Set the readyPump flag
+        if (strncmp(cmd, "readyPump", cmdLength) == 0) {
+            data->flags->readyPump = 1;
+        }
+
+        // Set the readyPump flag /****IF THIS SUPPOSED TO BE data->flags->readyPump = 0?****/
+        if (strncmp(cmd, "pumpDown", cmdLength) == 0) {
+            data->flags->readyPump = 1;
+        }
+
+        // Set the readyCommand flag
+        if (strncmp(cmd, "readyCommand", cmdLength) == 0) {
+            data->flags->readyCommand = 1;
+        }
+
+        // Set the propulse flag
+        if (strncmp(cmd, "propulse", cmdLength) == 0) {
+            data->flags->propulse = 1;
+        }
+
+        // Set the emergencyBrake flag
+        if (strncmp(cmd, "emergencyBrake", cmdLength) == 0) {
+            data->flags->emergencyBrake = 1;
+        }
+
+        // Turn on the MCU latch
+        if (strncmp(cmd, "mcuLatchOn", cmdLength) == 0) {
+            setMCULatch(true);
+        }
+
+        // Turn off the MCU latch
+        if (strncmp(cmd, "mcuLatchOff", cmdLength) == 0) {
+            setMCULatch(false);
+        }
+
+        // "enPrecharge"
+        if (strncmp(cmd, "enPrecharge", cmdLength) == 0) {
+            //pthread_create(&hbT, NULL, hbLoop, NULL);*/
+            rmsEnHeartbeat();
+            rmsClrFaults();
+            rmsInvDis();
+            //noTorqueMode = true;
+        }
+
+        // "cmdTorque"
+        if (strncmp(cmd, "cmdTorque", cmdLength) == 0) {
+            setMotorEn();
+        }
+
+        // Enable high voltage
+        if (strncmp(cmd, "hvEnable", cmdLength) == 0) {
+            /* Lets add a safety check here */
+            setMCUHVEnabled(true);
+        }
+
+        // Disable high voltage
+        if (strncmp(cmd, "hvDisable", cmdLength) == 0) {
+            setMCUHVEnabled(false);
+        }
+
+        // This doens't do anything...
+//        // Override the current state with the number which follows override
+//        if (strncmp(cmd, "override", 8) == 0){
+//            fprintf(stderr, "Override received for state: %s\n", cmd+9);
+//            sprintf(stateMachine.overrideStateName, "%s%c", cmd+9, '\0');
+//        }
+
+        // Heartbeat so that the dashboard can know the TCP server is still active
+        if (strncmp(cmd, "ping", cmdLength) == 0) {
+            // Send acknowledge packet back
+            socket -> send((char*) "pong1", 5);
+        }
+
+        /* Should we be concerned about this?
+         *	if (strncmp(cmd, "power off", cmdLength) == 0) {
+         *		// DO POWER OFF
+         *	}
+        */
+
+        /* Is this any different than the override command?
+         * if (strncmp(cmd, "state", 5) == 0) {
+         *   *lastPacket[HV] = getuSTimestamp();
+         *   data->state = cmd[5] - 0x30;
+         * }
+        */
+
+        // Resets nav and sets the readyToBrake flag
+        if (strncmp(cmd, "clrMotion", cmdLength) == 0) {
+            resetNav();
+            data->flags->readyToBrake = true;
+        }
+
+        // Enables the brakes on the pod
+        if (strncmp(cmd, "brake", cmdLength) == 0) {
+            //data->flags->shouldBrake = true;
+            brake();
+        }
+
+        // Actuates the primary brakes
+        if (strncmp(cmd, "primBrakeOn", cmdLength) == 0) {
+            //data->flags->brakePrimAct = true;
+            brakePrimaryActuate();
+        }
+
+        // Unactuates the primary brakes
+        if (strncmp(cmd, "primBrakeOff", cmdLength) == 0) {
+            //data->flags->brakePrimRetr = true;
+            brakePrimaryUnactuate();
+        }
+
+        // Actuates the secondary brakes
+        if (strncmp(cmd, "secBrakeOn", cmdLength) == 0) {
+            //data->flags->brakeSecAct = true;
+            brakeSecondaryActuate();
+        }
+
+        // Unactuates the secondary brakes
+        if (strncmp(cmd, "secBrakeOff", cmdLength) == 0) {
+            //data->flags->brakeSecRetr = true;
+            brakeSecondaryUnactuate();
+        }
+
     }
 
     cout << "TCP connection TERMINATED with " << socket -> getLocalAddress() << ":" << socket -> getLocalPort() << ".\n";
@@ -99,7 +235,7 @@ void *HandleConnection(void *arg) {
 
     // End connection
     delete socket;
-    return NULL;
+    return nullptr;
 }
 
 
