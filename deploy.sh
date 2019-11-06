6ea4a7407ec3bef030a76ca605ccb85e6eebe9d1
erfreeman <ezra@Ezra-PC.localdomain>
2019-11-05 20:41:57 -0600

    added error message

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+
 mkdir build && cd build
 cmake ..
 retVal=$?
 if [ $retVal -ne 0 ]; then
+	echo "Failure detected during cmake."
 	exit 1
 fi
 make
 retVal=$?
 if [ $retVal -ne 0 ]; then
+	echo "Failure detected during make."
 	exit 1
 fi
 exit 0
