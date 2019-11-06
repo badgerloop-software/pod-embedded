b4a8840f8e2dc4c729c07718aa0125f61382a146
erfreeman <ezra@Ezra-PC.localdomain>
2019-11-05 20:58:50 -0600

    formatted the if statements

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,23 @@
 
 if [ $1 == 'build']
 then
-
+	mkdir build && cd build
+	
+	cmake ..
+	retVal=$?
+	if [ $retVal -ne 0 ]
+	then
+		echo "Failure detected during cmake."
+		exit 1
+	fi
+	
+	make
+	retVal=$?
+	if [ $retVal -ne 0 ]
+	then
+		echo "Failure detected during make."
+		exit 1
+	fi
 elif [ $1 == 'clean']
 then
 
@@ -10,17 +26,4 @@ else
 	echo "Invalid argument: please put it in the form "./deploy.sh build" or "./deploy.sh clean""
 fi
 
-mkdir build && cd build
-cmake ..
-retVal=$?
-if [ $retVal -ne 0 ]; then
-	echo "Failure detected during cmake."
-	exit 1
-fi
-make
-retVal=$?
-if [ $retVal -ne 0 ]; then
-	echo "Failure detected during make."
-	exit 1
-fi
 exit 0
