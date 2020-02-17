78728102f8e6dad445c88820872835b31c5839c1
Daniel Kouchekinia <kouchekinia@wisc.edu>
2020-02-17 10:11:01 -0600

    Added namespace usage signifiers to the top of State cpp classes to avoid repetitive looking definitions.

--- embedded/app/state-machine/IdleState.cpp
+++ embedded/app/state-machine/IdleState.cpp
@@ -1,13 +1,15 @@
 #include "States.h"
 
-States::IdleState::IdleState(State *previousState) : State(previousState) {
+using namespace States;
+
+IdleState::IdleState(State *previousState) : State(previousState) {
     this -> numberOfRuns = 0;
 }
 
-State* States::IdleState::run(std::ostream *logStream) {
+State* IdleState::run(std::ostream *logStream) {
     return (this->numberOfRuns++ < 5 ? reinterpret_cast<State *> (this) : new States::TestState(this));
 }
 
-char* States::IdleState::getName() {
+char* IdleState::getName() {
     return "Idle State";
 }
--- embedded/app/state-machine/TestState.cpp
+++ embedded/app/state-machine/TestState.cpp
@@ -1,13 +1,15 @@
 #include "States.h"
 
-States::TestState::TestState(State *previousState) : State(previousState) {
+using namespace States;
+
+TestState::TestState(State *previousState) : State(previousState) {
     this -> numberOfRuns = 0;
 }
 
-State* States::TestState::run(std::ostream *logStream) {
+State* TestState::run(std::ostream *logStream) {
     return this->numberOfRuns++ < 5 ? this : nullptr;
 }
 
-char* States::TestState::getName() {
+char* TestState::getName() {
     return "Test State";
 }
