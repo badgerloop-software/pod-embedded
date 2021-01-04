02cc97e5247e3f3a33d54b4f0ef44b8699a94fd0
NothinButJohn <jharkins2@wisc.edu>
2021-01-04 13:35:20 -0600

    test beginnings

--- embedded/peripherals/src/Test_imu.cpp
+++ embedded/peripherals/src/Test_imu.cpp
@@ -10,16 +10,15 @@
 #include <imu.h>
 #include <data.h>
 
+
+class ImuTest : public ::testing::Test {
+    protected: void SetUp() override {
+        SetupIMU(); // setupIMU from imu.h
+    }
+};
+
+
+
 TEST(imuTest, VerifyWorkingGTest){
-    initData();
-    SetupIMU();
-    float deltaVelocityData[3];
-			getDeltaVData(deltaVelocityData);
-			
-			float accelData[3];
-			getAccelData(accelData);
-			
-			printf("Delta X: %f, Delta Y: %f, Delta Z: %f\n", deltaVelocityData[0], deltaVelocityData[1], deltaVelocityData[2]);
-			printf("Accel X: %f, Accel Y: %f, Accel Z: %f\n", accelData[0], accelData[1], accelData[2]);
 
 }
