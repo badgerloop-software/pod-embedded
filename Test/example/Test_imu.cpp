e35a61cdc01610c917fd0a506755c4293f9c2875
NothinButJohn <jharkins2@wisc.edu>
2021-01-10 11:31:12 -0600

    zeroes test, fix nav.c to .cpp next

--- Test/example/Test_imu.cpp
+++ Test/example/Test_imu.cpp
@@ -12,15 +12,19 @@
 class ImuTest : public ::testing::Test {
     protected: 
     void SetUp() override {
-        initData();
-        IMU imu;
+        // initData();
         imu.SetupIMU();
     }
-
+    IMU imu;
+    float deltaVelocityData[3];
+    float accelData[3];
 
 };
 
 // testing to see if fixture works, beginning
 TEST_F(ImuTest, Zeros){
-
+    imu.getDeltaVData(deltaVelocityData);
+    EXPECT_EQ(deltaVelocityData[0], 0);
+    EXPECT_EQ(deltaVelocityData[1], 0);
+    EXPECT_EQ(deltaVelocityData[2], 0);
 }
