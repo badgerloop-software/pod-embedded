d88710d566a44c32c9a037d4df63f0060fa6da74
Marco Kurzynski <mkurzynski@wisc.edu>
2021-01-10 23:41:39 +0000

    added #else for NOI2C

--- Test/components/HV_Iox/Test_HV_Iox.cpp
+++ Test/components/HV_Iox/Test_HV_Iox.cpp
@@ -8,16 +8,21 @@ TEST(HV_IOX, VerifyHVIox) {
     hv_iox.init(true);
     EXPECT_EQ(hv_iox.setupIox(), 0);
     #ifdef NOI2C
-        EXPECT_EQ(hv_iox.setMCULatch(true), 0);
         EXPECT_EQ(hv_iox.isHVIndicatorEnabled(), 1);
+    #else
+        hv_iox.setMCUHVEnabled(false);
+        EXPECT_NE(hv_iox.isHVIndicatorEnabled(), -1);
     #endif
-    hv_iox.setMCUHVEnabled(false);
-    EXPECT_NE(hv_iox.isHVIndicatorEnabled(), -1);
 }
 
 TEST(HV_IOX, HVIOX_MCU_LATCH) {
     HVIox hv_iox;
     hv_iox.init(true);
-    EXPECT_EQ(hv_iox.setMCULatch(true), 1);
-    EXPECT_EQ(hv_iox.setMCULatch(false), 1);
+    #ifdef NOI2C
+        EXPECT_EQ(hv_iox.setMCULatch(true), 0);
+        EXPECT_EQ(hv_iox.setMCULatch(false), 0);
+    #else
+        EXPECT_EQ(hv_iox.setMCULatch(true), 1);
+        EXPECT_EQ(hv_iox.setMCULatch(false), 1);
+    #endif
 }
