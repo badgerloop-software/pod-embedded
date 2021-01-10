c3dd9cb35579c71a8b15d31bb0c30dcc5c662a70
Marco Kurzynski <mkurzynski@wisc.edu>
2021-01-10 17:58:56 +0000

    added setMCULatch and isHVIndicatorEnabled testers with #ifdef NOI2C

--- Test/components/HV_Iox/Test_HV_Iox.cpp
+++ Test/components/HV_Iox/Test_HV_Iox.cpp
@@ -7,4 +7,12 @@ TEST(ComponentTests, VerifyHVIox) {
     HVIox hv_iox;
     hv_iox.init(true);
     EXPECT_EQ(hv_iox.setupIox(), 0);
+    #ifdef NOI2C
+        EXPECT_EQ(hv_iox.setMCULatch(true), 0);
+        EXPECT_EQ(hv_iox.isHVIndicatorEnabled(), 1);
+    #endif
+    hv_iox.setMCUHVEnabled(false);
+    EXPECT_EQ(hv_iox.setMCULatch(true), 1);
+    EXPECT_EQ(hv_iox.setMCULatch(false), 1);
+    EXPECT_NE(hv_iox.isHVIndicatorEnabled(), -1);
 }
