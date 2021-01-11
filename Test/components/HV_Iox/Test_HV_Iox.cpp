e72e1e4629dbbc37ccdcbae9e7bd3ebf189ec199
Marco Kurzynski <mkurzynski@wisc.edu>
2021-01-11 14:24:13 -0600

    added tests plz correct if wrong so I don't keep going

--- Test/components/HV_Iox/Test_HV_Iox.cpp
+++ Test/components/HV_Iox/Test_HV_Iox.cpp
@@ -1,5 +1,4 @@
 // Author: Marco
-// just to learn
 #include <gtest/gtest.h>
 #include <hv_iox.h>
 
@@ -7,22 +6,37 @@ TEST(HV_IOX, VerifyHVIox) {
     HVIox hv_iox;
     hv_iox.init(true);
     EXPECT_EQ(hv_iox.setupIox(), 0);
-    #ifdef NOI2C
-        EXPECT_EQ(hv_iox.isHVIndicatorEnabled(), 1);
-    #else
-        hv_iox.setMCUHVEnabled(false);
-        EXPECT_NE(hv_iox.isHVIndicatorEnabled(), -1);
-    #endif
+#ifdef NOI2C
+    EXPECT_EQ(hv_iox.isHVIndicatorEnabled(), 1);
+#else
+    hv_iox.setMCUHVEnabled(false);
+    EXPECT_NE(hv_iox.isHVIndicatorEnabled(), -1);
+#endif
 }
 
 TEST(HV_IOX, HVIOX_MCU_LATCH) {
     HVIox hv_iox;
     hv_iox.init(true);
-    #ifdef NOI2C
-        EXPECT_EQ(hv_iox.setMCULatch(true), 0);
-        EXPECT_EQ(hv_iox.setMCULatch(false), 0);
-    #else
-        EXPECT_EQ(hv_iox.setMCULatch(true), 1);
-        EXPECT_EQ(hv_iox.setMCULatch(false), 1);
-    #endif
+#ifdef NOI2C
+    EXPECT_EQ(hv_iox.setMCULatch(true), 0);
+    EXPECT_EQ(hv_iox.setMCULatch(false), 0);
+#else
+    EXPECT_EQ(hv_iox.setMCULatch(true), 1);
+    EXPECT_EQ(hv_iox.setMCULatch(false), 1);
+#endif
+}
+
+TEST(HV_IOX, HVIOX_STATES) {
+    HVIox hv_iox;
+    hv_iox.init(true);
+#ifdef NOI2C
+    EXPECT_EQ(hv_iox.getBMSMultiIn(), 1);
+    EXPECT_EQ(hv_iox.getIMDStatus(), 1);
+    EXPECT_EQ(hv_iox.getINRTStatus(), 1);
+#else
+    EXPECT_NE(hv_iox.getBMSMultiIn(), -1);
+// Not sure what the expected behavior should be... is it just a true bool?
+//  EXPECT_EQ(hv_iox.getIMDStatus(), -1);
+    EXPECT_EQ(hv_iox.getINRTStatus(), -1);
+#endif
 }
