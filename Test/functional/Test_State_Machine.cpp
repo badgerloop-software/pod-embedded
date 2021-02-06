14a80aa05d9ade11c513b5fbaf16375702f9efd3
Eric Udlis <udlis.eric@gmail.com>
2021-02-06 12:49:33 -0600

    Carry over some setup from the old state test

--- Test/functional/Test_State_Machine.cpp
+++ Test/functional/Test_State_Machine.cpp
@@ -10,9 +10,9 @@
 #include <unistd.h>
 
 extern "C" {
-    #include <states.h>
-    #include <state_machine.h>
-    #include <data.h>
+#include <data.h>
+#include <state_machine.h>
+#include <states.h>
 }
 
 #define WAIT(x) (usleep((x)*1000000.0))
@@ -44,90 +44,111 @@ static int checkForChange(char* name);
 
 class StateTest : public testing::Test {
 
-    void SetUp() {
+    void SetUp()
+    {
         // Executes before test
+        initData();
+        buildStateMachine();
+        /*    genericInit();*/
+        if (sem_init(&smSem, 0, 1) != 0) {
+            FAIL();
+        }
+        if (pthread_create(&smThread, NULL, &StateTest::stateMachineLoop, NULL) != 0)
+            FAIL();
     }
 
-    void TearDown() {
-    // Executes after test
+    void TearDown()
+    {
+        // Executes after test
     }
 
-    void genericInit(char* name) {
+    void genericInit(char* name)
+    {
         BANNER(name);
-    goToState(IDLE_NAME);
+        goToState(IDLE_NAME);
 
-    setFlagsReadyPump(0);
-    setFlagsPumpDown(0);
-    setFlagsReadyCommand(0);
-    setFlagsPropulse(0);
-    setFlagsEmergencyBrake(0);
-    setFlagsShouldStop(0);
-    setFlagsShutdown(0);
+        setFlagsReadyPump(0);
+        setFlagsPumpDown(0);
+        setFlagsReadyCommand(0);
+        setFlagsPropulse(0);
+        setFlagsEmergencyBrake(0);
+        setFlagsShouldStop(0);
+        setFlagsShutdown(0);
 
-    setTimersStartTime(getuSTimestamp());
-    setTimersOldRetro(0);
-    setTimersLastRetro(0);
+        setTimersStartTime(getuSTimestamp());
+        setTimersOldRetro(0);
+        setTimersLastRetro(0);
 
-    setPressurePrimTank(1500);
-    setPressurePrimLine(200);
-    setPressurePrimAct(1);
-    setPressureSecTank(1500);
-    setPressureSecLine(200);
-    setPressureSecAct(1);
-    setPressurePv(14);
+        setPressurePrimTank(1500);
+        setPressurePrimLine(200);
+        setPressurePrimAct(1);
+        setPressureSecTank(1500);
+        setPressureSecLine(200);
+        setPressureSecAct(1);
+        setPressurePv(14);
 
-    setMotionPos(0);
-    setMotionVel(0);
-    setMotionAccel(0);
-    setMotionRetroCount(0);
+        setMotionPos(0);
+        setMotionVel(0);
+        setMotionAccel(0);
+        setMotionRetroCount(0);
 
-    setBmsPackCurrent(0);
-    setBmsPackVoltage(270);
-    setBmsPackDCL(0);
-    setBmsPackCCL(0);
-    setBmsPackResistance(0);
-    setBmsPackHealth(100);
-    setBmsPackOpenVoltage(0);
-    setBmsPackCycles(0);
-    setBmsPackAh(0);
-    setBmsInputVoltage(0);
-    setBmsSoc(95);
-    setBmsRelayStatus(0);
-    setBmsHighTemp(25);
-    setBmsLowTemp(20);
-    setBmsCellMaxVoltage(3200); /* These three are mV */
-    setBmsCellMinVoltage(3000);
-    setBmsCellAvgVoltage(3100);
-    setBmsMaxCells(72);
-    setBmsNumCells(72);
+        setBmsPackCurrent(0);
+        setBmsPackVoltage(270);
+        setBmsPackDCL(0);
+        setBmsPackCCL(0);
+        setBmsPackResistance(0);
+        setBmsPackHealth(100);
+        setBmsPackOpenVoltage(0);
+        setBmsPackCycles(0);
+        setBmsPackAh(0);
+        setBmsInputVoltage(0);
+        setBmsSoc(95);
+        setBmsRelayStatus(0);
+        setBmsHighTemp(25);
+        setBmsLowTemp(20);
+        setBmsCellMaxVoltage(3200); /* These three are mV */
+        setBmsCellMinVoltage(3000);
+        setBmsCellAvgVoltage(3100);
+        setBmsMaxCells(72);
+        setBmsNumCells(72);
 
-    setRmsIgbtTemp(23);
-    setRmsGateDriverBoardTemp(22);
-    setRmsControlBoardTemp(21);
-    setRmsMotorTemp(25);
-    setRmsMotorSpeed(0);
-    setRmsPhaseACurrent(0);
-    setRmsPhaseBCurrent(0);
-    setRmsPhaseCCurrent(0);
-    setRmsDcBusVoltage(0);
-    setRmsLvVoltage(13);
-    setRmsCanCode1(0);
-    setRmsCanCode2(0);
-    setRmsFaultCode1(0);
-    setRmsFaultCode2(0);
-    setRmsCommandedTorque(0);
-    setRmsActualTorque(0);
-    setRmsRelayState(0);
-    setRmsElectricalFreq(0);
-    setRmsDcBusCurrent(0);
-    setRmsOutputVoltageLn(0);
-    setRmsVSMCode(0);
-    setRmsKeyMode(0);
+        setRmsIgbtTemp(23);
+        setRmsGateDriverBoardTemp(22);
+        setRmsControlBoardTemp(21);
+        setRmsMotorTemp(25);
+        setRmsMotorSpeed(0);
+        setRmsPhaseACurrent(0);
+        setRmsPhaseBCurrent(0);
+        setRmsPhaseCCurrent(0);
+        setRmsDcBusVoltage(0);
+        setRmsLvVoltage(13);
+        setRmsCanCode1(0);
+        setRmsCanCode2(0);
+        setRmsFaultCode1(0);
+        setRmsFaultCode2(0);
+        setRmsCommandedTorque(0);
+        setRmsActualTorque(0);
+        setRmsRelayState(0);
+        setRmsElectricalFreq(0);
+        setRmsDcBusCurrent(0);
+        setRmsOutputVoltageLn(0);
+        setRmsVSMCode(0);
+        setRmsKeyMode(0);
     }
 
+    static void* stateMachineLoop(void* context)
+    {
+        while (1) {
+            sem_wait(&smSem);
+            runStateMachine();
+            sem_post(&smSem);
+            WAIT(.1);
+        }
+        return NULL;
+    }
     stateMachine_t stateMachine;
 };
 
-TEST_F(StateTest, CreateState) {
-
+TEST_F(StateTest, CreateState)
+{
 }
