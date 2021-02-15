06cebe00f1063ca2f53fa6b369f768c7d79b4671
Marco Kurzynski <mkurzynski@wisc.edu>
2021-02-14 18:51:19 -0600

    Created new arg for deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 fileName=${0##*/}
 if [ "$#" -ne 1 ]; then
 	echo "one argument expected after './$fileName'"
-	echo "Options are: build, cross-setup, cross, clean, gtest-setup"
+	echo "Options are: build, cross-setup, cross, clean, gtest-setup, setup"
 	exit 1
 elif [ "$1" == "build" ]; then
     ./deploy.sh clean
@@ -24,6 +24,7 @@ elif [ "$1" == "build" ]; then
 		exit 1
 	fi
 	echo "build finished"
+
 elif [ "$1" == "cross-setup" ]; then
 	cd /tmp/
 	wget -c https://releases.linaro.org/components/toolchain/binaries/6.5-2018.12/arm-linux-gnueabihf/gcc-linaro-6.5.0-2018.12-x86_64_arm-linux-gnueabihf.tar.xz
@@ -53,13 +54,8 @@ elif [ "$1" == "cross" ]; then
 		exit 1
 	fi
 	echo "cross build finished"
+
 elif [ "$1" == "gtest-setup" ]; then
-	echo "Installing Dependencies (Assumes Debian based System) - Sudo access required"
-	sudo apt-get update
-	sudo apt-get install -y build-essential
-	sudo apt-get install -y cmake libgtest-dev
-    sudo apt-get install -y python3 python3-pip python-is-python3 clang-format
-	
 	echo "Building GTest"
 	cd /usr/src/gtest
 	sudo cmake CMakeLists.txt
@@ -67,6 +63,20 @@ elif [ "$1" == "gtest-setup" ]; then
 	sudo cp *.a /usr/lib
 	
 	echo "GTest Install Complete"
+
+elif [ "$1" == "setup" ]; then
+	echo "Installing Dependencies (Assumes Debian based System) - Sudo access required"
+	sudo apt-get update
+	sudo apt-get install -y build-essential
+	sudo apt-get install -y cmake libgtest-dev
+    sudo apt-get install -y python3 python3-pip python-is-python3 clang-format
+	
+	if [ $? -ne 0 ]; then
+		echo "failure detected during dependency installation"
+		exit 1
+	fi
+	echo "setup finished"
+
 elif [ "$1" == "format" ]; then
 	find . -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec clang-format -style=file -i {} \;
 	echo "Format successful"
