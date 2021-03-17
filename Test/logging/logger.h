37a2f36b30997b9770ca8bf254a6898a28bf99e2
NothinButJohn <jharkins2@wisc.edu>
2021-03-16 19:55:52 -0500

    log func

--- Test/logging/logger.h
+++ Test/logging/logger.h
@@ -1,5 +1,38 @@
 #include <chrono>
 #include <atomic>
+#include <string>
+#include <iostream>
+
+#pragma once
+
+#define LOGGER_LEVEL_INFO 0
+#define LOGGER_LEVEL_WARN 1
+#define LOGGER_LEVEL_ERROR 2
+
+#if !defined(LOGGER_ACTIVE_LEVEL)
+#define LOGGER_ACTIVE_LEVEL LOGGER_LEVEL_INFO
+#endif
+
+
+
+// Log Level Enum
+namespace level {
+    enum level_enum : int
+    {
+        info = LOGGER_LEVEL_INFO,
+        warn = LOGGER_LEVEL_WARN,
+        error = LOGGER_LEVEL_ERROR
+    };
+
+// #ifndef LOGGER_LEVEL_NAMES
+// #define LOGGER_LEVEL_NAMES
+//     {
+//         "info", "warn", "error"
+//     }
+// #endif
+
+
+} // namespace level
 
 namespace badger_logger {
     class stopwatch {
@@ -16,6 +49,18 @@ namespace badger_logger {
         void reset()
         {
             start_timepoint = log_clock::now();
+        }
+    };
+
+    class logger {
+        public: logger(std::string name)
+        {
+            std::string logger_name = name;
+        }
+
+        void log(std::string message, level::level_enum lvl)
+        {
+            std::wcout << message.c_str();
         }
     };
 } // nameespace badger_logger
