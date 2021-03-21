12b6f82ca87fa3325f2f289e7a48de5974937a26
Marco Kurzynski <mkurzynski@wisc.edu>
2021-03-21 02:11:15 +0000

    updated test

--- embedded/examples/gpioIOXRickMortyTest.cpp
+++ embedded/examples/gpioIOXRickMortyTest.cpp
@@ -10,19 +10,32 @@ extern "C" {
 
 iox_morty ioxMorty;
 iox_rick ioxRick;
+int testDirs(int dir, i2c_settings iox) {
+    printf("IIC settings:\nbus: %d\ndeviceAddress: %c\nopenMode %d",dir, iox.bus, iox.deviceAddress, iox.openMode);
+    printf("Setting dir to %d\n",dir);
+    for (int i = 0; i < 16; i++) {
+        setState(&iox, i, dir);
+        setDir(&iox, i, dir);
+    }
+}
 int main()
 {
     initData();
     if (ioxRick.init(false) != 0)
-        std::count << "ioxRick doesn't return 0" << std::endl;
+            printf("ioxRick.init returned !0");
     i2c_settings iox = ioxRick.getiox_rickDev();
+    testDirs(1, iox);
     printf("---Showing IOX_RICK---\n");
     for (int i = 0; i < 16; i++) {
         printf("PIN: %d, VAL: %d, DIR: %d\n", i, getState(&iox, i), getDir(&iox, i));
     }
 
-    ioxMorty.init(false);
+
+
+    if (ioxMorty.init(false))
+            printf("ioxMorty return !0");
     iox = ioxMorty.getiox_mortyDev();
+    testDirs(0, iox);
     printf("---Showing IOX_MORTY---\n");
     for (int i = 0; i < 16; i++) {
         printf("PIN: %d, VAL: %d, DIR: %d\n", i, getState(&iox, i), getDir(&iox, i));
